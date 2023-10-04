c40d60ae59de94a95489f4265ad83b383ddab4bb
weizetan <131359684+weizetan@users.noreply.github.com>
2023-10-04 15:59:51 +1100

    homework2 file uploaded

---
+++ homework2/vega_lite.js
@@ -0,0 +1,30 @@
+var vg_1 = "js/total_countries.json";
+vegaEmbed("#total_countries", vg_1)
+    .then(function (result) {})
+    .catch(console.error);
+
+var vg_2 = "js/maxscore.json";
+    vegaEmbed("#maxscore", vg_2)
+        .then(function (result) {})
+        .catch(console.error);
+
+var vg_3 = "js/averagescore.json";
+    vegaEmbed("#averagescore", vg_3)
+        .then(function (result) {})
+        .catch(console.error);
+
+var vg_4 = "js/minscore.json";
+        vegaEmbed("#minscore", vg_4)
+            .then(function (result) {})
+            .catch(console.error);
+
+var vg_5 = "js/map.json";
+vegaEmbed("#map", vg_5)
+    .then(function (result) {})
+    .catch(console.error);
+
+var vg_6 = "js/scatterplot1.json";
+vegaEmbed("#scatterplot1", vg_6)
+    .then(function (result) {})
+    .catch(console.error);
+
