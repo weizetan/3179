d33cc5c7772726af511d91435cfe323140c5752a
weizetan <131359684+weizetan@users.noreply.github.com>
2023-10-07 15:21:03 +1100

    update html and vegalite.js

--- a2/vega_lite.js
+++ a2/vega_lite.js
@@ -1,4 +1,50 @@
-var vg_1 = "js/map.json";
-vegaEmbed("#map", vg_1)
+var vg_1 = "js/total_countries.json";
+vegaEmbed("#total_countries", vg_1)
     .then(function (result) {})
     .catch(console.error);
+
+var vg_2 = "js/maxscore.json";
+    vegaEmbed("#maxscore", vg_2)
+        .then(function (result) {})
+        .catch(console.error);
+
+var vg_3 = "js/averagescore.json";
+    vegaEmbed("#averagescore", vg_3)
+        .then(function (result) {})
+        .catch(console.error);
+
+var vg_4 = "js/minscore.json";
+        vegaEmbed("#minscore", vg_4)
+            .then(function (result) {})
+            .catch(console.error);
+
+var vg_5 = "js/map.json";
+vegaEmbed("#map", vg_5)
+    .then(function (result) {})
+    .catch(console.error);
+
+var vg_6 = "js/top5.json";
+    vegaEmbed("#top5", vg_6)
+        .then(function (result) {})
+        .catch(console.error);
+
+var vg_7 = "js/last5.json";
+vegaEmbed("#last5", vg_7)
+    .then(function (result) {})
+    .catch(console.error);
+
+var vg_8 = "js/scatterplot1.json";
+vegaEmbed("#scatterplot1", vg_8)
+    .then(function (result) {})
+    .catch(console.error);
+
+var vg_9 = "js/scatterplots.json";
+vegaEmbed("#scatterplots", vg_9)
+        .then(function (result) {})
+        .catch(console.error);
+
+var vg_10 = "js/barchart.json";
+vegaEmbed("#barchart", vg_10)
+        .then(function (result) {})
+        .catch(console.error);
+
