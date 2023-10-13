212f032f3d66dbb166a0018845d8b55fcc29c3b1
weizetan <131359684+weizetan@users.noreply.github.com>
2023-10-13 12:01:02 +1100

    updated final version

--- a2/vega_lite.js
+++ a2/vega_lite.js
@@ -1,50 +1,50 @@
 var vg_1 = "js/total_countries.json";
-vegaEmbed("#total_countries", vg_1)
+vegaEmbed("#total_countries", vg_1, {renderer: "svg", actions: false})
     .then(function (result) {})
     .catch(console.error);
 
 var vg_2 = "js/maxscore.json";
-    vegaEmbed("#maxscore", vg_2)
+    vegaEmbed("#maxscore", vg_2, {renderer: "svg", actions: false})
         .then(function (result) {})
         .catch(console.error);
 
 var vg_3 = "js/averagescore.json";
-    vegaEmbed("#averagescore", vg_3)
+    vegaEmbed("#averagescore", vg_3, {renderer: "svg", actions: false})
         .then(function (result) {})
         .catch(console.error);
 
 var vg_4 = "js/minscore.json";
-        vegaEmbed("#minscore", vg_4)
+        vegaEmbed("#minscore", vg_4, {renderer: "svg", actions: false})
             .then(function (result) {})
             .catch(console.error);
 
 var vg_5 = "js/map.json";
-vegaEmbed("#map", vg_5)
+vegaEmbed("#map", vg_5, {renderer: "svg", actions: false})
     .then(function (result) {})
     .catch(console.error);
 
 var vg_6 = "js/top5.json";
-    vegaEmbed("#top5", vg_6)
+    vegaEmbed("#top5", vg_6, {renderer: "svg", actions: false})
         .then(function (result) {})
         .catch(console.error);
 
 var vg_7 = "js/last5.json";
-vegaEmbed("#last5", vg_7)
+vegaEmbed("#last5", vg_7, {renderer: "svg", actions: false})
     .then(function (result) {})
     .catch(console.error);
 
 var vg_8 = "js/scatterplot1.json";
-vegaEmbed("#scatterplot1", vg_8)
+vegaEmbed("#scatterplot1", vg_8, {renderer: "svg", actions: false})
     .then(function (result) {})
     .catch(console.error);
 
 var vg_9 = "js/scatterplots.json";
-vegaEmbed("#scatterplots", vg_9)
+vegaEmbed("#scatterplots", vg_9, {renderer: "svg", actions: false})
         .then(function (result) {})
         .catch(console.error);
 
 var vg_10 = "js/barchart.json";
-vegaEmbed("#barchart", vg_10)
+vegaEmbed("#barchart", vg_10, {renderer: "svg", actions: false})
         .then(function (result) {})
         .catch(console.error);
 
